64ab7adf32d75d54d1b31be486614ab0abe83396
Son Le <itsonla@ghtk.co>
2023-07-06 19:38:33 +0700

    Init proj

---
+++ MyAnswer/MyAnswer/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  MyAnswer
+//
+//  Created by Son Le on 06/07/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
