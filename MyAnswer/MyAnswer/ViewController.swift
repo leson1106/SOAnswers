ca930bdd159b4bda244b4a1f0409c9ec45a8b5c9
Son Le <itsonla@ghtk.co>
2023-08-18 11:37:08 +0700

    Mock

--- MyAnswer/MyAnswer/ViewController.swift
+++ MyAnswer/MyAnswer/ViewController.swift
@@ -7,9 +7,69 @@
 
 import UIKit
 
+//Related to: https://stackoverflow.com/questions/76926122/how-to-handle-two-completion-handlers-in-one-function-swift
 class ViewController: UIViewController {
+    
+    private let apiCaller = APICaller()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        fetch { data in
+            //TODO:
+        }
+    }
+    
+    func fetch(completionHandler: @escaping (([String: Any]) -> Void)) {
+        apiCaller.fetchUser { data, error in
+            let group = DispatchGroup()
+            
+            group.enter()
+            self.apiCaller.fetchSlideOnSaleEditorChoiceNewArrivalData(baseData: data) { _, _ in
+                //Do something like decoding data
+                print("Fetched slide")
+                group.leave()
+            }
+            
+            group.enter()
+            self.apiCaller.fetchProducts(baseData: data) { _, _ in
+                //Do something like decoding data
+                print("Fetched product")
+                group.leave()
+            }
+            
+            group.notify(queue: .main) {
+                //Group data if needed then completion
+                //completionHandler...
+                print("Completed")
+            }
+            
+            group.leave()
+        }
+    }
+}
+
+struct OnSaleItem { }
+struct SlideItem { }
+struct NewArrivalItem { }
+struct EditorChoiceItem { }
+
+class APICaller {
+    func fetchUser(completionHandler: @escaping ((_ data: [String: Any], _ error: Error?) -> Void)) {
+        completionHandler([:], nil)
+    }
+    
+    func fetchSlideOnSaleEditorChoiceNewArrivalData(baseData: [String: Any],
+                                                    completionHandler: @escaping ((_ data: [String: Any], _ error: Error?) -> Void)) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            completionHandler([:], nil)
+        }
+    }
+    
+    func fetchProducts(baseData: [String: Any],
+                       completionHandler: @escaping ((_ data: [String: Any], _ error: Error?) -> Void)) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            completionHandler([:], nil)
+        }
     }
 }
