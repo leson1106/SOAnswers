fceb28631859887e727c55acf429443368b61915
Son Le <itsonla@ghtk.co>
2023-10-20 21:48:20 +0700

    Added

--- MyAnswer/MyAnswer/AppDelegate.swift
+++ MyAnswer/MyAnswer/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         let vc = ViewController()
-        window?.rootViewController = vc
+        let nav = UINavigationController(rootViewController: vc)
+        window?.rootViewController = nav
         window?.makeKeyAndVisible()
         
         return true
--- MyAnswer/MyAnswer/ViewController.swift
+++ MyAnswer/MyAnswer/ViewController.swift
@@ -7,9 +7,67 @@
 
 import UIKit
 
+class ParentClass {
+    deinit {
+        print("DEINIT PARENT")
+    }
+    
+    lazy var child: ChildClass = ChildClass()
+    
+    init() { }
+}
+
+class ChildClass {
+    deinit {
+        print("DEINIT CHILD")
+    }
+    
+    lazy var parent: ParentClass = ParentClass()
+    
+    init() { }
+}
+
 class ViewController: UIViewController {
+    
+    private let nameLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(nameLabel)
+        
+        view.backgroundColor = .blue
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(presentVC)))
+    }
+    
+    @objc private func presentVC() {
+        let vc = ViewController2()
+        vc.completion = { text in
+            self.nameLabel.text = text
+            print("Completion get called")
+        }
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
+class ViewController2: UIViewController {
+    
+    var parentC = ParentClass()
+    var child = ChildClass()
+    
+    var completion: ((String) -> ())?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        
+        parentC.child = child
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        completion?("Did Appear")
     }
 }
