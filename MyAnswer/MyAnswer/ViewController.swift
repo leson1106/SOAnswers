d88543d865f94626b59a30fe37b4096c4752ad94
Son Le <itsonla@ghtk.co>
2023-10-21 14:18:43 +0700

    Added

--- MyAnswer/MyAnswer/ViewController.swift
+++ MyAnswer/MyAnswer/ViewController.swift
@@ -13,3 +13,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
 }
+
+//https://stackoverflow.com/questions/67615581/how-to-create-a-custom-uilabel-in-swift-with-a-specific-color/
+class MyCustomLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        // This will call `awakeFromNib` in your code
+        setup()
+    }
+    
+    private func setup() {
+        self.textColor = .red
+    }
+}
